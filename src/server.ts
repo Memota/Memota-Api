c9014d3a31779ab09cae9cf85e6533ab4dd6a2b0
Nico Franke <nico.franke01@gmail.com>
2021-05-28 12:46:53 +0200

    Add important header middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
 import Koa from "koa"
+import cors from "@koa/cors"
 
 import { config } from "./config"
+import helmet from "koa-helmet"
 
 const app = new Koa()
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    dnsPrefetchControl: false,
+    expectCt: true,
+    frameguard: true,
+    hidePoweredBy: false,
+    hsts: true,
+    ieNoOpen: false,
+    noSniff: true,
+    permittedCrossDomainPolicies: true,
+    referrerPolicy: false,
+    xssFilter: false,
+  }),
+)
+
+app.use(cors)
 
 app.use(ctx => {
   ctx.body = "Hello World"
